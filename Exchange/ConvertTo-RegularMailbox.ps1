8daf97e69cf0332b217119069d4c88574a6c2964
Rafael Medeiros <rafael.medeiros@valtech.com>
2020-04-13 22:26:28 -0300

    ConvertTo-RegularMailbox.ps1

---
+++ Exchange/ConvertTo-RegularMailbox.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Script to Convert a Shared Mailbox into a Regular Mailbox
+.DESCRIPTION
+    This script will convert a shared mailbox into a regular mailbox using the ExoPowershellModule.
+.NOTES
+    Author: Rafael de Jesus Medeiros <rafael_medeiros94@hotmail.com.br>
+    GitHub: https://github.com/RafaelM1994/Powershell-Scripts/
+    Creation Date: 02/10/2020
+    
+#>
+
+$user = "firstname.lastname@company.com"
+
+#Exchange credentials
+try{
+    $IsActiveSession = (Get-Mailbox -ResultSize 1 -WarningAction silentlyContinue ).name.length -gt 0
+}catch{
+
+}
+
+if ($IsActiveSession){
+    write-host "[INFO]EXO Session OK." -foregroundcolor green
+}else{
+    clear-host
+    Write-host "Connecting to the Exchange Service, 
+    please insert your credentials in the window that can be behind the current one..." -foregroundcolor yellow
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    Install-Module -Name Microsoft.Exchange.Management.ExoPowershellModule -Confirm:$false
+    $SesionO365 = New-EXOPSSession
+    Import-PSSession $SesionO365 -AllowClobber
+}
+
+#Convert User mailbox to a shared mailbox
+Set-Mailbox $user -Type:Regular -Verbose
